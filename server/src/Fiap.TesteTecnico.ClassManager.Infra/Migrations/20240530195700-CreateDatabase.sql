450ed449ba191779022864808faa11b427b13896
lukkanog <lukanog@hotmail.com>
2024-05-30 21:29:58 -0300

    feat: add database schema migration

---
+++ server/src/Fiap.TesteTecnico.ClassManager.Infra/Migrations/20240530195700-CreateDatabase.sql
@@ -0,0 +1,30 @@
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'aluno')
+BEGIN
+    CREATE TABLE aluno (
+        id INT PRIMARY KEY,
+        nome VARCHAR(255),
+        usuario VARCHAR(45),
+        senha CHAR(60)
+    );
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'turma')
+BEGIN
+    CREATE TABLE turma (
+        id INT PRIMARY KEY,
+        curso_id INT,
+        turma VARCHAR(45),
+        ano INT
+    );
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'aluno_turma')
+BEGIN
+    CREATE TABLE aluno_turma (
+        aluno_id INT FOREIGN KEY REFERENCES aluno(id),
+        turma_id INT FOREIGN KEY REFERENCES turma(id)
+    );
+END
+GO
