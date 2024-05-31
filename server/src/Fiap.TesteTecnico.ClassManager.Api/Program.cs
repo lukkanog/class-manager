d9fd72939ab8fb60503a9c6912a7531ff7456319
lukkanog <lukanog@hotmail.com>
2024-05-30 22:00:47 -0300

    feat: setup Serilog

--- server/src/Fiap.TesteTecnico.ClassManager.Api/Program.cs
+++ server/src/Fiap.TesteTecnico.ClassManager.Api/Program.cs
@@ -1,17 +1,25 @@
 using Fiap.TesteTecnico.ClassManager.Infra;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
