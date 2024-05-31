58196abeba39105fb3af1e4b67395ec7980f6133
lukkanog <lukanog@hotmail.com>
2024-05-30 21:34:56 -0300

    fix: change primary keys to have IDENTITY behavior

--- server/src/Fiap.TesteTecnico.ClassManager.Infra/Migrations/20240530195700-CreateDatabase.sql
+++ server/src/Fiap.TesteTecnico.ClassManager.Infra/Migrations/20240530195700-CreateDatabase.sql
@@ -1,7 +1,7 @@
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'aluno')
 BEGIN
     CREATE TABLE aluno (
-        id INT PRIMARY KEY,
+        id INT IDENTITY PRIMARY KEY,
         nome VARCHAR(255),
         usuario VARCHAR(45),
         senha CHAR(60)
@@ -12,7 +12,7 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'turma')
 BEGIN
     CREATE TABLE turma (
-        id INT PRIMARY KEY,
+        id INT IDENTITY PRIMARY KEY,
         curso_id INT,
         turma VARCHAR(45),
         ano INT
