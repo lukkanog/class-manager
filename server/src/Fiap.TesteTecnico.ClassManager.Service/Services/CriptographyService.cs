96e0082134e7fdf22ee526aadd9f08084e6ea4ca
lukkanog <lukanog@hotmail.com>
2024-06-01 12:31:17 -0300

    fix: change criptography method to match sql column's max length

--- server/src/Fiap.TesteTecnico.ClassManager.Service/Services/CriptographyService.cs
+++ server/src/Fiap.TesteTecnico.ClassManager.Service/Services/CriptographyService.cs
@@ -7,7 +7,7 @@ public static class CriptographyService
 {
     public static string HashPassword(string password)
     {
-        byte[] bytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+        byte[] bytes = SHA1.HashData(Encoding.UTF8.GetBytes(password));
 
         StringBuilder builder = new StringBuilder();
 
