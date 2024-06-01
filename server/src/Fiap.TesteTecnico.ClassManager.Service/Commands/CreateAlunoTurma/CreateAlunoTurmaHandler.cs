dcfc56ddefbdaefc58b6a29e8700d1d549226161
lukkanog <lukanog@hotmail.com>
2024-06-01 14:28:17 -0300

    feat: add validator for creation of AlunoTurma relation

--- server/src/Fiap.TesteTecnico.ClassManager.Service/Commands/CreateAlunoTurma/CreateAlunoTurmaHandler.cs
+++ server/src/Fiap.TesteTecnico.ClassManager.Service/Commands/CreateAlunoTurma/CreateAlunoTurmaHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Fiap.TesteTecnico.ClassManager.Domain.Dto;
 using Fiap.TesteTecnico.ClassManager.Domain.Exceptions;
 using Fiap.TesteTecnico.ClassManager.Domain.Interfaces.Repositories;
+using Fiap.TesteTecnico.ClassManager.Service.Extensions;
+using FluentValidation;
 using MediatR;
 
 namespace Fiap.TesteTecnico.ClassManager.Service.Commands.CreateAlunoTurma;
@@ -8,15 +10,21 @@ namespace Fiap.TesteTecnico.ClassManager.Service.Commands.CreateAlunoTurma;
 public class CreateAlunoTurmaHandler(
     IAlunoTurmaRepository alunoTurmaRepository,
     IAlunoRepository alunoRepository,
-    ITurmaRepository turmaRepository
+    ITurmaRepository turmaRepository,
+    IValidator<CreateAlunoTurmaCommand> validator
 ) : IRequestHandler<CreateAlunoTurmaCommand, AlunoTurmaDto>
 {
     private readonly IAlunoTurmaRepository _alunoTurmaRepository = alunoTurmaRepository;
     private readonly IAlunoRepository _alunoRepository = alunoRepository;
     private readonly ITurmaRepository _turmaRepository = turmaRepository;
+    private readonly IValidator<CreateAlunoTurmaCommand> _validator = validator;
 
     public async Task<AlunoTurmaDto> Handle(CreateAlunoTurmaCommand request, CancellationToken cancellationToken)
     {
+        var result = await _validator.ValidateAsync(request);
+
+        result.ThrowExceptionIfNotValid("Erro ao associar aluno e turma");
+
         var existingAluno = await _alunoRepository.GetByIdAsync(request.AlunoId);
         if (existingAluno is null)
             throw new NotFoundException($"Aluno de id {request.AlunoId} não foi encontrado.");
---
+++ server/src/Fiap.TesteTecnico.ClassManager.Service/Validators/CreateAlunoTurmaCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using Fiap.TesteTecnico.ClassManager.Domain.Interfaces.Repositories;
+using Fiap.TesteTecnico.ClassManager.Service.Commands.CreateAlunoTurma;
+using FluentValidation;
+
+namespace Fiap.TesteTecnico.ClassManager.Service.Validators;
+public class CreateAlunoTurmaCommandValidator
+    : AbstractValidator<CreateAlunoTurmaCommand>
+{
+    private readonly IAlunoTurmaRepository _alunoTurmaRepository;
+
+    public CreateAlunoTurmaCommandValidator(IAlunoTurmaRepository alunoTurmaRepository)
+    {
+        _alunoTurmaRepository = alunoTurmaRepository;
+
+        RuleFor(x => new { x.AlunoId, x.TurmaId })
+            .MustAsync(async (model, _, cancellationToken) =>
+            {
+                var existing = await _alunoTurmaRepository.GetByAlunoIdAndTurmaIdAsync(model.AlunoId, model.TurmaId);
+                return existing is null;
+            })
+            .WithMessage("Aluno já está relacionado a essa turma");
+    }
+
+}
