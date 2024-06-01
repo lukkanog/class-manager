61aa5c2b9418d0c9bee05ce86b0c7833b14f459a
lukkanog <lukanog@hotmail.com>
2024-06-01 05:06:04 -0300

    feat: check existence of turma and aluno before inserting new

--- server/src/Fiap.TesteTecnico.ClassManager.Service/Commands/CreateAlunoTurma/CreateAlunoTurmaHandler.cs
+++ server/src/Fiap.TesteTecnico.ClassManager.Service/Commands/CreateAlunoTurma/CreateAlunoTurmaHandler.cs
@@ -1,15 +1,30 @@
 ﻿using Fiap.TesteTecnico.ClassManager.Domain.Dto;
+using Fiap.TesteTecnico.ClassManager.Domain.Exceptions;
 using Fiap.TesteTecnico.ClassManager.Domain.Interfaces.Repositories;
 using MediatR;
 
 namespace Fiap.TesteTecnico.ClassManager.Service.Commands.CreateAlunoTurma;
 
-public class CreateAlunoTurmaHandler(IAlunoTurmaRepository alunoTurmaRepository) : IRequestHandler<CreateAlunoTurmaCommand, AlunoTurmaDto>
+public class CreateAlunoTurmaHandler(
+    IAlunoTurmaRepository alunoTurmaRepository,
+    IAlunoRepository alunoRepository,
+    ITurmaRepository turmaRepository
+) : IRequestHandler<CreateAlunoTurmaCommand, AlunoTurmaDto>
 {
     private readonly IAlunoTurmaRepository _alunoTurmaRepository = alunoTurmaRepository;
+    private readonly IAlunoRepository _alunoRepository = alunoRepository;
+    private readonly ITurmaRepository _turmaRepository = turmaRepository;
 
     public async Task<AlunoTurmaDto> Handle(CreateAlunoTurmaCommand request, CancellationToken cancellationToken)
     {
+        var existingAluno = await _alunoRepository.GetByIdAsync(request.AlunoId);
+        if (existingAluno is null)
+            throw new NotFoundException($"Aluno de id {request.AlunoId} não foi encontrado.");
+
+        var existingTurma = await _turmaRepository.GetByIdAsync(request.TurmaId);
+        if (existingTurma is null)
+            throw new NotFoundException($"Turma de id {request.TurmaId} não foi encontrado.");
+
         var created = await _alunoTurmaRepository.AddAsync(request);
 
         return new AlunoTurmaDto(created.AlunoId, created.TurmaId);
