8a1fe47fef407b052dc3cd37f21782e11b18be23
lukkanog <lukanog@hotmail.com>
2024-06-02 01:51:54 -0300

    feat: create Alunos list page using MudBlazor

--- Fiap.TesteTecnico.ClassManager.WebApp/Program.cs
+++ Fiap.TesteTecnico.ClassManager.WebApp/Program.cs
@@ -1,11 +1,16 @@
 using Fiap.TesteTecnico.ClassManager.WebApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiRoute = builder.Configuration.GetSection("ApiRoute").Value
+    ?? builder.HostEnvironment.BaseAddress;
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiRoute) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
